1a74f9aedded364623e342b4e4b629d76f5bbeb8
Bobur Sobirjanov <sobirjanovbobur768@gmail.com>
2024-02-14 15:15:24 +0500

    swagger added to project

--- src/main/java/parkingmanagement/config/BeanConfig.java
+++ src/main/java/parkingmanagement/config/BeanConfig.java
@@ -1,6 +1,9 @@
 package parkingmanagement.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +14,24 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class BeanConfig {
     @Bean
+    public OpenAPI customizeOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+    @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setSkipNullEnabled(true);
         return modelMapper;
     }
-
     @Bean
     public PasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();
     }
--- src/main/java/parkingmanagement/domain/dto/order/OrderCreateDto.java
+++ src/main/java/parkingmanagement/domain/dto/order/OrderCreateDto.java
@@ -1,9 +1,7 @@
 package parkingmanagement.domain.dto.order;
 
 import lombok.*;
-import parkingmanagement.domain.entity.cars.CarType;
 
-import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
